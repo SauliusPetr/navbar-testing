21614ad359b5d7129d6aead2d38777c4bc442d1f
Saulius <petruseviciuss@gmail.com>
2024-02-26 20:03:05 +0200

    add hamburger navigation

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function openBurger(){
+    let options = document.querySelector('.hamburger-navigation .options');
+    if(options.classList.contains('open-burger')){
+        options.classList.toggle('open-burger');
+    }else{
+        options.classList.toggle('open-burger');
+    }
+    
+}
