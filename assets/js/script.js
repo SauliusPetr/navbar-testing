74a3e9bc451a49ebe6c46d2b4f2111941eec79af
Saulius <petruseviciuss@gmail.com>
2024-03-07 15:04:09 +0200

    slider functionality, navbar transition fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,38 @@
-function openBurger(){
-    let options = document.querySelector('.hamburger-navigation .options');
-    if(options.classList.contains('open-burger')){
-        options.classList.toggle('open-burger');
-    }else{
-        options.classList.toggle('open-burger');
+function openBurger() {
+  let options = document.querySelector(".navigation .hamburger-options");
+  if (options.classList.contains("open-burger")) {
+    options.classList.toggle("open-burger");
+  } else {
+    options.classList.toggle("open-burger");
+  }
+}
+function openDropdown() {
+  let options = document.querySelector(".navigation .dropdown-options");
+  if (options.classList.contains("open-dropdown")) {
+    options.classList.toggle("open-dropdown");
+  } else {
+    options.classList.toggle("open-dropdown");
+  }
+}
+
+function nextSlide() {
+  let imageContainer = document.querySelectorAll(".image-container");
+  imageContainer.forEach((image) => {
+    if (image.style.transform == "translateX(-100%)") {
+      image.style.transform = "translateX(0)";
+    } else {
+      image.style.transform = "translateX(100%)";
     }
-    
+  });
+}
+
+function previousSlide() {
+  let imageContainer = document.querySelectorAll(".image-container");
+  imageContainer.forEach((image) => {
+    if (image.style.transform == "translateX(100%)") {
+      image.style.transform = "translateX(0)";
+    } else {
+      image.style.transform = "translateX(-100%)";
+    }
+  });
 }
